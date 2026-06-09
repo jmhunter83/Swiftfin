243b4fae9f74430e4e3b136e8290f31e57bc8d63
Jacob M Hunter <jmhunter@gmail.com>
2026-06-09 11:35:31 -0700

    chore: add license header to ATS check script

--- Scripts/Checks/ValidateATSConfiguration.swift
+++ Scripts/Checks/ValidateATSConfiguration.swift
@@ -1,5 +1,13 @@
 #!/usr/bin/env swift
 
+//
+// Swiftfin is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, you can obtain one at https://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) 2026 Jellyfin & Jellyfin Contributors
+//
+
 import Darwin
 import Foundation
 
